5c90476d496ef955648e934575c06f91bf459164
SEIKH NABAB UDDIN <93948993+nababuddin@users.noreply.github.com>
2023-10-24 12:31:35 +0530

    Update current_weather.py

--- web_programming/current_weather.py
+++ web_programming/current_weather.py
@@ -1,30 +1,21 @@
 import requests
 
-APPID = ""  # <-- Put your OpenWeatherMap appid here!
-URL_BASE = "https://api.openweathermap.org/data/2.5/"
+API_KEY = ""  # <-- Put your free Weatherstack API key here!
+URL_BASE = "http://api.weatherstack.com/current"
 
-
-def current_weather(q: str = "Chicago", appid: str = APPID) -> dict:
-    """https://openweathermap.org/api"""
-    return requests.get(URL_BASE + "weather", params=locals()).json()
-
-
-def weather_forecast(q: str = "Kolkata, India", appid: str = APPID) -> dict:
-    """https://openweathermap.org/forecast5"""
-    return requests.get(URL_BASE + "forecast", params=locals()).json()
-
-
-def weather_onecall(lat: float = 55.68, lon: float = 12.57, appid: str = APPID) -> dict:
-    """https://openweathermap.org/api/one-call-api"""
-    return requests.get(URL_BASE + "onecall", params=locals()).json()
-
+def current_weather(location: str, api_key: str = API_KEY) -> dict:
+    params = {
+        "access_key": api_key,
+        "query": location
+    }
+    return requests.get(URL_BASE, params=params).json()
 
 if __name__ == "__main__":
     from pprint import pprint
 
     while True:
-        location = input("Enter a location:").strip()
+        location = input("Enter a location (city name): ").strip()
         if location:
-            pprint(current_weather(location))
+            pprint(current_weather(location, API_KEY))
         else:
             break
